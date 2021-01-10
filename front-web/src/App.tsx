0f916b0d10fec6963a5941251b7251f6e1ffa7a0
unknown <lindsoncardoso.al@gmail.com>
2021-01-10 13:17:22 -0300

    Add navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
-import Hello from './Hello'
+import './App.css'
+
+import Navbar from './Navbar';
 function App() {
   return (
     <div>
-      Hellow
+      <Navbar/>
     </div>
   );
 }
