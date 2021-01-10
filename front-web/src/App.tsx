c1b9f6b6b2fcde9a5a1f72a3eba8317cdd636c0e
unknown <lindsoncardoso.al@gmail.com>
2021-01-10 14:17:51 -0300

    add Routes and Stepsheader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
-import Navbar from './Navbar';
-import Home from './Home'
+
+
+import Routes from './routes';
 function App() {
   return (
     <>
-      <Navbar/>
-      <Home/>
+      <Routes/>
     </>
   );
 }
