72143ae3a90a6665817f269124ecfe7756e22c49
unknown <lindsoncardoso.al@gmail.com>
2021-01-11 00:41:24 -0300

    finnish 3.1

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -7,13 +7,13 @@ function Footer(){
         <footer className="main-footer" >
         App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="" target="_new">
+                <a href="https://www.youtube.com/channel/UC3twHmWQwtqEO7u-gB_2f7g" target="_new">
                     <YoutubeIcon/>
                 </a>
                 <a href="linkedin.com/in/lindson-cardoso-739334169" target="_new">
                     <LinkedinIcon/>
                 </a>
-                <a target="_new">
+                <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
                 <InstagramIcon/>
                 </a>
 
